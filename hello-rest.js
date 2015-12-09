db67224ba6e2f6a3cf4b4f55889430d02ed64d60
gogsadmin <dummy@gmail.com>
2015-12-09 13:16:50 +0000

    Updated page hello-rest.js

--- hello-rest.js
+++ hello-rest.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello fabric8\n');
+  res.end('Hello AWESOME\n');
 }).listen(8080);
 console.log('Server running at http://localhost:8080/');
